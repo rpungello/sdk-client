894e5714199a5b5a594c5ae54d29bad1a7cb73ce
Rob Pungello <rob@pdfsystems.com>
2024-09-20 14:48:10 -0400

    Improve version.sh

--- version.sh
+++ version.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Enter version number: "
+# Read the current version from composer.json
+CURRENT_VERSION=$(jq -r ".version" composer.json)
 
-read VERSION
-jq ".version=\"$VERSION\"" --indent 4 composer.json >composer.json.new
-mv composer.json.new composer.json
+# Read the increment argument (if provided)
+INCREMENT=$1
 
-git add composer.json
-git commit -qm "Bump version to $VERSION"
-git tag -a -m "Tagging version $VERSION" "$VERSION"
+# If $INCREMENT is not empty, run semver to increment the version, then update composer.json and create a git tag
+if [ -n "$INCREMENT" ]; then
+    VERSION=$(semver --increment "$INCREMENT" "$CURRENT_VERSION")
+    jq ".version=\"$VERSION\"" --indent 4 composer.json >composer.json.new
+    mv composer.json.new composer.json
+
+    git add composer.json
+    git commit -qm "Bump version to $VERSION"
+    git tag -a -m "Tagging version $VERSION" "$VERSION"
+else
+    echo "No increment provided, skipping version bump"
+    exit 1
+fi
